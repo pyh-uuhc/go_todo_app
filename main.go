6306cd66a829916bffb354ae2daf3be0de27883f
youngha <pyh.uuhc@gmail.com>
2024-07-09 20:41:46 +0900

    add comments

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func run(ctx context.Context) error {
 			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
 		}),
 	}
-	s.ListenAndServe()
+
 	eg, ctx := errgroup.WithContext(ctx)
 	eg.Go(func() error {
 		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
@@ -39,3 +39,11 @@ func run(ctx context.Context) error {
 
 	return eg.Wait()
 }
+
+// 채널에서 알림이 온 경우는 다음과 같은 순서로 run 함수가 종료된다.
+
+// - ctx.Done() 다음 줄의 *http.Server.Shutdown 메서드가 실행된다.
+// - 다른 고루틴에서 실행되던 *http.Server.ListenAndServe 메서드가 종료된다.
+// - 다른 고루틴에서 실행되던 익명 함수(func() error)가 종료된다.
+// - run 함수의 마지막 부분에서 다른 고루틴이 종료되는 것을 기다리던 *errgroup.Group.Wait 메서드가 종료된다.
+// - 다른 고루틴에서 실행되던 익명 함수(func() error)의 반환값이 run 함수의 반환값이 된다.
