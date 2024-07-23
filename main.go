ceff35bf4651f5fc42192b2f461a48f8b7782514
youngha <pyh.uuhc@gmail.com>
2024-07-23 14:05:27 +0900

    fix typo

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ func run(ctx context.Context) error {
 	}
 	l, err := net.Listen("tcp", fmt.Sprintf(":%d", cfg.Port))
 	if err != nil {
-		log.Fatal("failed to listen port %d: %v", cfg.Port, err)
+		log.Fatalf("failed to listen port %d: %v", cfg.Port, err)
 	}
 	url := fmt.Sprintf("http://%s", l.Addr().String())
 	log.Printf("start with: %v", url)
